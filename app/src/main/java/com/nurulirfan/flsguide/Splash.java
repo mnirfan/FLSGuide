3c15cdf6d0f5cb3f24dfd4a5c72da296ecbf1a8c
idindrakusuma <indrakusuma.udinus@gmail.com>
2017-05-14 21:18:16 +0700

    add splashscreen & change icon launcher

---
+++ app/src/main/java/com/nurulirfan/flsguide/Splash.java
@@ -0,0 +1,29 @@
+package com.nurulirfan.flsguide;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L); //3000 L = 3 detik
+    }
+}
